7a7203b89e71aba4c39ee7ba3930290be9455d5d
Bandit Silachai <mbandit001@yahoo.co.th>
2022-06-13 20:21:31 +0700

    hope it work 🚀

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+require('dotenv').config();
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
 var app = express();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
+  let detailFile = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  }
+  console.log(detailFile);
+  res.json(detailFile)
+})
 
 
 
